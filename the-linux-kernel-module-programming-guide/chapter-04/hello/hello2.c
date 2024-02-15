02da81dc1190e85dfc900a3862a75a2debe08376
ryagi <ryan@smartcow.ai>
2024-02-15 08:14:52 +0100

    added more examples

---
+++ the-linux-kernel-module-programming-guide/chapter-04/hello/hello2.c
@@ -0,0 +1,27 @@
+/*
+ * Demo of using the module_init() and module_exit() macros.
+ *
+ * Author: Ryan Agius
+ *
+ */
+
+#include <linux/init.h> /* Needed for the macros */
+#include <linux/module.h> /* Needed by all modules */
+#include <linux/printk.h> /* Needed for pr_info() */
+
+static int __init hello_2_init(void)
+{
+    pr_info("Hello, world 2\n");
+    return 0;
+}
+
+static void __exit hello_2_exit(void)
+{
+    pr_info("Goodbye cruel world!\n");
+}
+
+module_init(hello_2_init);
+module_exit(hello_2_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ryan Agius");
